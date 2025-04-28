ec2c3b0cfd4c9eaa8b28c05b3abdae863254a9ee
Ann18532 <anupsavaliya28@gmail.com>
2025-04-28 14:52:46 +0000

    union

--- Assignment4/output/output.s
+++ Assignment4/output/output.s
@@ -1,14 +1,12 @@
 .data
 newline: .asciiz "\n"
-str0: .asciiz ""
-str1: .asciiz ""
 .text
 .globl main
 main:
     move $fp, $sp
-    addi $sp, $sp, -32
-    sw   $ra, 28($sp)
-    sw   $fp, 24($sp)
+    addi $sp, $sp, -20
+    sw   $ra, 16($sp)
+    sw   $fp, 12($sp)
     #Pushing a to stack
     addi $t8, $sp, 0
     #Loading constant 5 into register
@@ -19,35 +17,8 @@ main:
     #Loading constant 10 into register
     li $t9, 10
     sw $t9, 0($t7)
-    li $t6, 4
-    la $a0, str0
-    move $a1, $t6
-#scanf
-    li $v0, 4
-syscall
-    li $v0, 5
-syscall
-move $t6 ,$v0
-    move $t5, $v0
-    la $a0, str1
-    move $a1, $t6
-#printf
- move $a0, $a0
-    li $v0, 4
-syscall
-  li $v0, 4 
- la $a0, newline 
- syscall
- move $a0, $a1
-    li $v0, 1
-syscall
-  li $v0, 4 
- la $a0, newline 
- syscall
-    move $t4, $v0
-    li $v0, 0
-    lw   $fp, 24($sp)
-    lw   $ra, 28($sp)
-    addi $sp, $sp, 32
+    lw   $fp, 12($sp)
+    lw   $ra, 16($sp)
+    addi $sp, $sp, 20
     li $v0, 10
     syscall
--- Assignment4/test/input1.c
+++ Assignment4/test/input1.c
@@ -1,4 +1,4 @@
-struct A{
+struct A {
     int x;
     int y;
 };
@@ -7,10 +7,4 @@ int main() {
     struct A a;
     a.x = 5;
     a.y = 10;
-    // int m=a.x;
-    int d = 4;
-    scanf("%d", &d);
-    printf("%d\n", d);
-
-    return 0;
 }
