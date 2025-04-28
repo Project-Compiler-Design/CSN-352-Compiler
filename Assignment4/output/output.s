94afde93d03b188267dfd01bc90992040adc0b93
BitBeast <adimun1004@gmail.com>
2025-04-28 19:41:03 +0530

    static

--- Assignment4/output/output.s
+++ Assignment4/output/output.s
@@ -1,53 +1,16 @@
 .data
 newline: .asciiz "\n"
-str0: .asciiz ""
-str1: .asciiz ""
 .text
 .globl main
 main:
     move $fp, $sp
-    addi $sp, $sp, -32
-    sw   $ra, 28($sp)
-    sw   $fp, 24($sp)
-    #Pushing a to stack
-    addi $t8, $sp, 0
-    #Loading constant 5 into register
-    li $t9, 5
-    sw $t9, 0($t8)
-    #Pushing a to stack
-    addi $t7, $sp, 4
-    #Loading constant 10 into register
-    li $t9, 10
-    sw $t9, 0($t7)
-    li $t6, 4
-    la $a0, str0
-    move $a1, $t6
-#scanf
-    li $v0, 4
-syscall
-    li $v0, 5
-syscall
-move $t6 ,$v0
-    move $t5, $v0
-    la $a0, str1
-    move $a1, $t6
-#printf
- move $a0, $a0
-    li $v0, 4
-syscall
-  li $v0, 4 
- la $a0, newline 
- syscall
- move $a0, $a1
-    li $v0, 1
-syscall
-  li $v0, 4 
- la $a0, newline 
- syscall
-    move $t4, $v0
+    addi $sp, $sp, -12
+    sw   $ra, 8($sp)
+    sw   $fp, 4($sp)
+    li $t9, 3
     li $v0, 0
-    lw   $fp, 24($sp)
-    lw   $ra, 28($sp)
-    addi $sp, $sp, 32
+    lw   $fp, 4($sp)
+    lw   $ra, 8($sp)
+    addi $sp, $sp, 12
     li $v0, 10
     syscall
--- Assignment4/test/input1.c
+++ Assignment4/test/input1.c
@@ -1,16 +1,7 @@
-struct A{
-    int x;
-    int y;
-};
 int main() {
     
-    struct A a;
-    a.x = 5;
-    a.y = 10;
-    // int m=a.x;
-    int d = 4;
-    scanf("%d", &d);
-    printf("%d\n", d);
+    static int a;
+    a=3;
 
     return 0;
 }
